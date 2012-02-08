479fcfd9abd1d96b276208b9740e5bde72ff6189
DanX <danx.exe@gmail.com>
2012-02-08 16:04:42 -0200

    infobip test deliver

--- lib/jung/drivers/infobip/campaign.rb
+++ lib/jung/drivers/infobip/campaign.rb
@@ -2,19 +2,28 @@ module Jung::Drivers::Infobip::Campaign
 
   attr_accessor :username, :password, :api_url, :messages_ids
 
-  def deliver
+  def deliver(recipients = self.recipients)
     messages_ids = recipients.map do | recipient |
-      deliver_recipient recipient.address, message
+      address = recipient.is_a?(Jung::Recipient) ? recipient.address : recipient
+      deliver_recipient address, message
     end
-    messages_ids.reduce(true) do | acc, value |
-      acc && value != false
-    end
+    delivery_success? messages_ids
+  end
+
+  def deliver_test(recipients)
+    deliver(recipients)
   end
 
   protected
 
   def deliver_recipient address, message
     api.send_sms address, message, sender.name
+  end
+
+  def delivery_success?(messages_ids)
+    messages_ids.reduce(true) do | acc, value |
+      acc && value != false
+    end
   end
 
 end
